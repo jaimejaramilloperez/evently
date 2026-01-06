6042fd30476487e015dcd35fb8c5d214dda06a24
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2026-01-06 15:49:10 -0500

    init: Initial project structure

---
+++ src/API/Evently.API/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => Results.Ok("0K"));
+
+app.Run();
