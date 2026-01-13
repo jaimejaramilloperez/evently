9e90304dae3e105dc67bb0e38d87d9c1b6832048
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2026-01-13 15:14:46 -0500

    feat: Add global exception handling

--- src/Api/Evently.Api/Program.cs
+++ src/Api/Evently.Api/Program.cs
@@ -1,7 +1,10 @@
+using System.Diagnostics;
 using Evently.Api.Extensions;
+using Evently.Api.Middlewares;
 using Evently.Common.Application;
 using Evently.Common.Infrastructure;
 using Evently.Modules.Events.Infrastructure;
+using Microsoft.AspNetCore.Http.Features;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddProblemDetails(options => options.CustomizeProblemDetails = context =>
+{
+    context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+    context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+
+    Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+    context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+});
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
 builder.Services.AddInfrastructure(builder.Configuration);
@@ -26,6 +40,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 app.UseSerilogRequestLogging();
 
