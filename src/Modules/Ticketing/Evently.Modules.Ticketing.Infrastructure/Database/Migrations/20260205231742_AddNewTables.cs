da105aeb26e075cbce83a62c4efdab6497eadb3e
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2026-02-05 18:23:51 -0500

    feat: Add new tables for events, orders, payments, and tickets in the ticketing module

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/Database/Migrations/20260205231742_AddNewTables.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evently.Modules.Ticketing.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "ticketing",
+                table: "customers",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuidv7()");
+
+            migrationBuilder.CreateTable(
+                name: "events",
+                schema: "ticketing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    starts_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ends_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    canceled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_events", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                schema: "ticketing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    total_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: false),
+                    tickets_issued = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_orders", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "payments",
+                schema: "ticketing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    order_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    transaction_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: false),
+                    amount_refunded = table.Column<decimal>(type: "numeric", nullable: true),
+                    created_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_payments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticket_types",
+                schema: "ticketing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: false),
+                    quantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    available_quantity = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_ticket_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tickets",
+                schema: "ticketing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    customer_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    order_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ticket_type_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "text", nullable: false),
+                    created_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    archived = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_tickets", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_items",
+                schema: "ticketing",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    order_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ticket_type_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    quantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_order_items", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_order_items_orders_order_id",
+                        column: x => x.order_id,
+                        principalSchema: "ticketing",
+                        principalTable: "orders",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_order_items_order_id",
+                schema: "ticketing",
+                table: "order_items",
+                column: "order_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events",
+                schema: "ticketing");
+
+            migrationBuilder.DropTable(
+                name: "order_items",
+                schema: "ticketing");
+
+            migrationBuilder.DropTable(
+                name: "payments",
+                schema: "ticketing");
+
+            migrationBuilder.DropTable(
+                name: "ticket_types",
+                schema: "ticketing");
+
+            migrationBuilder.DropTable(
+                name: "tickets",
+                schema: "ticketing");
+
+            migrationBuilder.DropTable(
+                name: "orders",
+                schema: "ticketing");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                schema: "ticketing",
+                table: "customers",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuidv7()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
